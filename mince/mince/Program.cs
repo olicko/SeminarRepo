f9bb2eed3c18ffaa4689cd8d7317793e979cef11
Norbert <suchy@gchd.cz>
2025-03-31 16:41:03 +0200

    přidávám mince a ProblemBatohuBacktracking

---
+++ mince/mince/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mince
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void ReturnValues()
+        {
+            string[] coinStr = Console.ReadLine().Split(' ');
+            int[] coin = new int[coinStr.Length];
+            for (int i = 0; i < coinStr.Length; i++)
+            {
+                coin[i] = Int32.Parse(coinStr[i]);
+            }
+            
+        }
+    }
+}
